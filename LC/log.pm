33bac3e2f7234f253260352e8f787c4f69179e9e
Damien Neil <neild@tlily.org>
1997-09-17 05:34:42 +0000

    *** empty log message ***

--- LC/log.pm
+++ LC/log.pm
@@ -2,11 +2,13 @@ package LC::log;
 
 use Exporter;
 use IO::File;
+use LC::time_stamp;
+
 @ISA = qw(Exporter);
 
-@EXPORT = qw(&log_notice &log_err &log_info &log_debug);
+@EXPORT = qw(&log_notice &log_err &log_info &log_debug $timestamp);
 
-
+tie $timestamp, 'LC::time_stamp';
 
 sub log_notice {
     main::ui_output("*** @_\n");
@@ -14,16 +16,16 @@ sub log_notice {
 
 sub log_err {
     main::ui_end();
-    print "*ERROR* @_\n";
+    print "*ERROR* $timestamp @_\n";
     exit(1);
 }
 
 
 sub log_info {
-    main::ui_output("*INFO* @_\n");
+    main::ui_output("*INFO* $timestamp @_\n");
 }
 
 sub log_debug {
     my $s=IO::File->new(">>/tmp/foo.log");
-    print $s "*DEBUG* @_\n";
+    print $s "*DEBUG* $timestamp @_\n";
 }
