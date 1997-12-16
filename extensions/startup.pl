e3ca2764159d114e2dc65630d5a3f3e3b6640d91
Chris Stevens <albert@tlily.org>
1997-12-16 09:53:48 +0000

    Remove $config{startup} var Change $HOME to $ENV{HOME} Change a log_notice to a ui_output

--- extensions/startup.pl
+++ extensions/startup.pl
@@ -1,21 +1,17 @@
 # -*- Perl -*-
 
-if($config{startup}) {
-    register_eventhandler(Type => 'connected',
-			  Order => 'after',
-			  Call => \&startup_handler
-			  );
-}
-
+register_eventhandler(Type => 'connected',
+		      Order => 'after',
+		      Call => \&startup_handler);
 
 sub startup_handler ($$) {
     my($event,$handler) = @_;
-    if(-f $HOME."/.lily/tlily/Startup") {
-        ui_output("(Sourcing ~/.lily/tlily/Startup)\n");
-	do $HOME."/.lily/tlily/Startup";
+    if(-f $ENV{HOME}."/.lily/tlily/Startup") {
+        ui_output("(Eval-ing ~/.lily/tlily/Startup)\n");
+	do $ENV{HOME}."/.lily/tlily/Startup";
 	ui_output("*** Error: ".$@) if $@;
     } else {
-        log_notice("(You may add perl code in ~/.lily/tlily/Startup)");
+        ui_output("(You may add perl code in ~/.lily/tlily/Startup)");
     }
     deregister_handler($handler->{Id});
     return 0;
