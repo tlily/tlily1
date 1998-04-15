57e4532dc20141841190549654503012db567b63
Steve Czetty <steve@tlily.org>
1998-04-15 04:45:48 +0000

    Added https to url.pl

--- extensions/url.pl
+++ extensions/url.pl
@@ -8,6 +8,7 @@ sub handler {
     my($event, $handler) = @_;
 
     $event->{Body} =~ s|(http://\S+)|push @urls, $1; "<url>$1</url>";|ge;
+	 $event->{Body} =~ s|(https://\S+)|push @urls, $1; "<url>$1</url>";|ge;
     $event->{Body} =~ s|(ftp://\S+)|push @urls, $1; "<url>$1</url>";|ge;
     return 0;
 }
