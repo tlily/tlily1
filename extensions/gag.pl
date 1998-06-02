f776475142e6966dcbe6bea6285954d85ece7322
Steve Czetty <steve@tlily.org>
1998-06-02 17:22:41 +0000

    Reworked %gag for ui_filter

--- extensions/gag.pl
+++ extensions/gag.pl
@@ -1,20 +1,21 @@
 # -*- Perl -*-
-# $Header: /data/cvs/tlily/extensions/gag.pl,v 1.5 1998/05/29 05:12:26 mjr Exp $
+# $Header: /data/cvs/tlily/extensions/gag.pl,v 1.6 1998/06/02 17:22:41 steve Exp $
 
 #
 # The gag extension adds the ability to `gag' all sends from a given user.
 #
+
+my %gagged;
 
 
 sub gag_command_handler($) {
     my($args) = @_;
 
     if ($args eq '') {
-	if (scalar(ui_gaglist()) == 0) {
+	if (scalar(keys(%gagged)) == 0) {
 	    ui_output("(no users are being gagged)");
 	} else {
-	    my @users=ui_gaglist(); 
-	    ui_output("(Gagged users: " . join(', ', @users). ")" );
+	    ui_output("(Gagged users: " . join(', ', keys(%gagged)). ")" );
 	}
 	return;
     }
@@ -25,16 +26,51 @@ sub gag_command_handler($) {
 	return;
     }
 
-    if (ui_gagged($name)) {
-	ui_ungag($name);
+    if ($gagged{$name}) {
+	delete $gagged{$name};
+	ui_resetfilter('gag');
 	ui_output("($name is no longer gagged.)");
     } else {
-	ui_gag($name);
+	$gagged{$name} = 1;
+	ui_resetfilter('gag');
 	ui_output("($name is now gagged.)");
     }
 
     return;
 }
+
+sub init() {
+    register_eventhandler( Type => 'send',
+    			   Order => 'before',
+			   Call => sub {
+	my ($event, $handler) = @_;
+
+	ui_output("eventhandler called!");
+
+	return 0 unless ((!$event->{First}) && $gagged{$event->{From}});
+
+	$event->{Text} = "<<gag>>$event->{From}:$event->{Text}<</gag>>";
+
+	return 0;
+    });
+
+    ui_filter('gag', sub {
+	my ($from, $text) = /(.*):(.*)/;
+
+	ui_output("gag.pl:mangler called.");
+	return $text unless ($from && $gagged{$from} && $text =~ /^\s*\-/);
+
+	my $new = $text;
+
+	$new =~ s/\b\w\b/m/g;
+	$new =~ s/\b\w\w\b/mm/g;
+	$new =~ s/\b\w\w\w\b/mrm/g;
+	$new =~ s/\b(\w+)\w\w\w\b/'m'.('r'x length($1)).'fl'/ge;
+
+	return $new;
+    });
+
+} 
 
 register_user_command_handler('gag', \&gag_command_handler);
 register_help_short('gag', 'affix a gag to a user');
