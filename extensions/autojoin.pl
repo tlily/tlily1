f8bd2ce65d5a69155fa6cf8e20344de321c42972
Chris Stevens <albert@tlily.org>
1998-06-12 05:38:45 +0000

    fix typo

--- extensions/autojoin.pl
+++ extensions/autojoin.pl
@@ -1,8 +1,8 @@
-# $Header: /data/cvs/tlily/extensions/autojoin.pl,v 1.2 1998/05/29 05:12:24 mjr Exp $
+# $Header: /data/cvs/tlily/extensions/autojoin.pl,v 1.3 1998/06/12 05:38:45 albert Exp $
 register_eventhandler(Type => 'disccreate',
     Call => sub {
 	my($e, $h) = @_;
-	if(!$e->Tags) {
+	if(!$e->{Tags}) {
 	    ui_output("(Auto-Joining $e->{Name})");
 	    dispatch_event({Type => 'userinput',
 			    Text => "/join $e->{Name}"});
